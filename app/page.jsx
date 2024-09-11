c6e14abc1d754d4b8be8c313f6996b30dbac5f9e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-09-11 15:59:29 -0400

    basic hero image styling finished

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,39 @@
 import HomeNavBar from "./ui/navbars/home_navbar/HomeNavBar";
+import styles from "./css/home.module.css";
+import Image from "next/image";
 import 'animate.css';
 
 export default function Home() {
   return (
     <>
-    <HomeNavBar />
-    
-    <main>
-      This is the home page
-    </main>
+      <HomeNavBar />
+
+      <main>
+        {/* Hero Image Section */}
+        <section className={styles.heroImgSection}>
+          <Image
+            className={`${styles.heroImg} animate__animated animate__fadeInDown`}
+            src='/img/homeHero.png'
+            height={1274}
+            width={1920}
+            alt="trees surrounding a lake with a mountain in the background"
+          />
+          <Image
+            className={`${styles.heroVector} animate__animated animate__fadeInLeft`}
+            src='/img/homeHeroVector.svg'
+            height={1274}
+            width={1920}
+            alt="vector image in the shape of a triangle"
+          />
+          <Image
+            className={`${styles.heroLogo} animate__animated animate__fadeIn`}
+            src='/img/heroLogo.png'
+            height={1274}
+            width={1920}
+            alt="to be with counselings brand logo"
+          />
+        </section>
+      </main>
     </>
   );
 }
--- app/ui/navbars/home_navbar/HomeNavBar.jsx
+++ app/ui/navbars/home_navbar/HomeNavBar.jsx
@@ -1,7 +1,6 @@
 'use client'
-
-import styles from "../../../css/home.module.css";
 import Link from "next/link";
+import styles from "../../../css/homeNavBar.module.css";
 import { useState } from "react";
 import Image from "next/image";
 
