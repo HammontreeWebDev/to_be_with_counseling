b5a2e090098bd957907be52ce72028b290e5ccc9
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-09-23 15:14:52 -0400

    added clouds effect using WebGL

--- app/(pages)/about/page.jsx
+++ app/(pages)/about/page.jsx
@@ -1,7 +1,12 @@
+'use client';
+import Clouds from "@/app/ui/about/Clouds";
+
 export default function about() {
     return(
         <>
         This is the about page
+        {/* This is where I'll import the ui component */}
+        <Clouds />
         </>
     )
 }
