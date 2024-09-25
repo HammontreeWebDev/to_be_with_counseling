8402d1996c366d98603ce343bc5d46d8fd19426b
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-09-25 14:47:27 -0400

    about page completed

--- app/(pages)/about/page.jsx
+++ app/(pages)/about/page.jsx
@@ -22,6 +22,15 @@ export default function about() {
             </section>
 
             <section className={`${styles.aboutContainer}`}>
+                {/* //! Background */}
+                <Image
+                    src={'/img/contentBackground.svg'}
+                    width={1720}
+                    height={1470}
+                    alt="background texture"
+                    className={`${styles.backgroundTexture}`}
+                />
+
                 {/* //!Title Row */}
                 <div className={`${styles.titleRow}`}>
                     <h1 className={`script-titles`}>Freedom</h1>
@@ -68,19 +77,19 @@ export default function about() {
                 {/* //!Content Column */}
 
                 <div className={`${styles.contentColumn}`}>
-                    <p className={`big-body-text`}>
+                    <p className={`big-body-text text-forest-green`}>
                         Karah is a Registered Mental Health Counselor Intern in the State of Florida who is passionate about walking alongside her clients in their search for long-lasting freedom from eating disorders, body image issues, and body shame. In assisting her clients uncover the root issues that have contributed to the development of an unhealthy relationship with food and body, she has witnessed the unequivocal transformation that occurs when one is able to come back home to themselves, utilizing compassion, kindness, and curiosity, in this needed restoration process.
                     </p>
 
-                    <p className={`big-body-text`}>
+                    <p className={`big-body-text text-forest-green`}>
                         Karah enjoys working with children, adolescents, young adults, and families within individual and group counseling. Along with eating disorders, body image issues, and body shame, Karah has experience working with individuals struggling with abuse, trauma, anxiety, depression, relationship difficulties, spirituality issues, and self-esteem issues. She utilizes a variety of evidence-based treatment approaches grounded in a foundation of attachment, as she believes when one is hurt in relationship, then healing must also take place in relationship.
                     </p>
 
-                    <p className={`big-body-text`}>
+                    <p className={`big-body-text text-forest-green`}>
                         Some of these theories include Embodiment, Internal Family Systems, Narrative Therapy, Dialectical Behavior Therapy, Acceptance Commitment Therapy, and Psychodynamic. While Karah is trained within these modalities, she also believes each individual is unique and therefore needs individualized treatment. She does this by meeting the client where they are, following the clients lead, and wisely providing direction when necessary.
                     </p>
 
-                    <p className={`big-body-text`}>
+                    <p className={`big-body-text text-forest-green`}>
                         Karah acknowledges that it can be difficult and uncomfortable for children and adolescents to sit and talk to someone about difficult topics. Therefore, she often utilizes a nature-based environment to intertwine the child’s natural desire to play with the needed mental support. Since this nature-based environment does take place on a therapy farm, the utilization of the farm animals often occurs, alongside play therapy, art therapy, equine-facilitated psychotherapy, and nature-based therapy.
                     </p>
 
