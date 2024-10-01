1219262981b37f5025f249218d62b1074f07f6c9
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-10-01 14:26:47 -0400

    fixed alignment issue

--- app/(pages)/counseling/page.jsx
+++ app/(pages)/counseling/page.jsx
@@ -1,25 +1,16 @@
 'use client';
 import styles from '@/app/css/counseling.module.css';
-import ShaderBackground from "@/app/ui/about/ShaderBackground";
-import Image from "next/image";
+// import ShaderBackground from "@/app/ui/about/ShaderBackground";
 
 export default function about() {
     return (
         <main>
             <section className={`${styles.titleContainer}`}>
 
-                {/* //* Background Image */}
-                <Image
-                    src={'/img/counselingTitleImage.png'}
-                    height={1920}
-                    width={1603}
-                    alt='house on a hill'
-                    className={`${styles.titleImage}`}
-                />
+                    <h1 className={`${styles.titleSlogan} script-titles text-center`}>
+                        Restoring your relationship with food leads you to restoring your relationship with yourself
+                    </h1>
 
-                <h1 className={`${styles.titleSlogan} script-titles text-center`}>
-                Restoring your relationship with food leads you to restoring your relationship with yourself
-                </h1>
             </section>
         </main>
     )
