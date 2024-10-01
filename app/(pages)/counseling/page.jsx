461fc428cd4fd36770442fc2f7e1766ddc124180
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-10-01 14:40:40 -0400

    added philosophy title

--- app/(pages)/counseling/page.jsx
+++ app/(pages)/counseling/page.jsx
@@ -6,11 +6,15 @@ export default function about() {
     return (
         <main>
             <section className={`${styles.titleContainer}`}>
-
                     <h1 className={`${styles.titleSlogan} script-titles text-center`}>
                         Restoring your relationship with food leads you to restoring your relationship with yourself
                     </h1>
-
+            </section>
+            
+            <section className={`${styles.philosophyContainer}`}>
+                <div className={`${styles.philosophyColumn}`}>
+                    <h2 className={`big-ovo-header text-center text-med-brown`}>Philosophy</h2>
+                </div>
             </section>
         </main>
     )
