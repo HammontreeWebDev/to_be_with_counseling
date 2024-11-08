50915b38a1eb37b64bf5b9b0bf395f35880dcde1
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-11-08 13:02:12 -0500

    updated styling for resources

--- app/ui/common/Resources.jsx
+++ app/ui/common/Resources.jsx
@@ -1,48 +1,59 @@
+import { useState } from "react";
 
-  export default function Resources({productList, title}) {
-    const products = productList
+export default function Resources({ productList, title }) {
+  const products = productList
+  const [isOpen, setIsOpen] = useState(false)
 
-    return (
-      <div>
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-xl font-bold text-gray-900">{title}</h2>
-  
-          <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
-              <div key={product.id}>
-                <div className="relative">
-                  <div className="relative h-72 w-full overflow-hidden rounded-lg">
-                    <img
-                      alt={product.imageAlt}
-                      src={product.imageSrc}
-                      className="h-full w-full object-cover object-center"
-                    />
-                  </div>
-                  <div className="relative mt-4">
-                    <h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
-                    <p className="mt-1 text-sm text-gray-500">{product.author}</p>
-                  </div>
-                  <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
-                    <div
-                      aria-hidden="true"
-                      className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
-                    />
-                    <p className="relative text-lg font-semibold text-white">{product.price}</p>
-                  </div>
-                </div>
-                <div className="mt-6">
-                  <a
-                    href={product.href}
-                    className="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200"
-                  >
-                    View On Amazon<span className="sr-only">, {product.name}</span>
-                  </a>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    )
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
   }
-  
+
+  return (
+    <div>
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <button className="customButton mx-auto max-w-2xl lg:max-w-7xl body-text" onClick={handleOpen}>{title}</button>
+
+        {
+          isOpen ?
+            <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+              {products.map((product) => (
+                <div key={product.id}>
+                  <div className="relative">
+                    <div className="relative h-72 w-full overflow-hidden rounded-lg">
+                      <img
+                        alt={product.imageAlt}
+                        src={product.imageSrc}
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
+                    <div className="relative mt-4">
+                      <h3 className="body-text text-[var(--forest-green)]">{product.name}</h3>
+                      <p className="mt-1 copyright text-[var(--medium-brown)]">{product.author}</p>
+                    </div>
+                    <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
+                      <div
+                        aria-hidden="true"
+                        className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
+                      />
+                      <p className="relative text-lg font-semibold text-white">{product.price}</p>
+                    </div>
+                  </div>
+                  <div className="mt-6">
+                    <a
+                      href={product.href}
+                      className="relative customButton flex items-center justify-center rounded-md border border-transparent body-text"
+                    >
+                      View On Amazon<span className="sr-only">, {product.name}</span>
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+            :
+            null
+        }
+
+      </div>
+    </div>
+  )
+}
