6404c6de70d1f9a66c0d11e0455a036b1b403730
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2025-01-06 14:00:04 -0500

    updated styling for counseling page on desktop versions

--- app/(pages)/counseling/page.jsx
+++ app/(pages)/counseling/page.jsx
@@ -8,7 +8,7 @@ export default function counseling() {
     return (
         <main>
             <section className={`${styles.titleContainer}`}>
-                <h1 className={`${styles.titleSlogan} script-titles text-center`}>
+                <h1 className={`${styles.titleSlogan} adjusted-script-titles text-center`}>
                     Restoring your relationship with food leads you to restoring your relationship with yourself
                 </h1>
             </section>
